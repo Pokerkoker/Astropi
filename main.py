07acd6e5e36f301f723fccdd1f1b834f11ba8df0
lukiluk447 <lukas.vlaeminck@vlot.be>
2026-02-11 16:55:57 +0100

    het werkt half maar krijg alleen 0 binnen

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import time
 from picamera import PiCamera
 from picamzero import Camera
+from sense_hat import SenseHat
 
 # image_1 = 'imgs/photo_393_53245738275_o.jpg'
 # image_2 = 'imgs/photo_394_53245245041_o.jpg'
@@ -38,11 +39,6 @@ def calculate_matches(descriptors_1, descriptors_2):
     return sorted(matches, key=lambda x: x.distance)
 
 
-image_1_cv, image_2_cv = convert_to_cv(image_1, image_2)
-kp1, kp2, des1, des2 = calculate_features(image_1_cv, image_2_cv, 500)
-matches = calculate_matches(des1, des2)
-
-
 def find_matching_coordinates(kp1, kp2, matches):
     c1, c2 = [], []
     for m in matches:
@@ -62,79 +58,117 @@ def calculate_mean_distance(c1, c2):
         distances.append(math.hypot(p1[0]-p2[0], p1[1]-p2[1]))
     return median(distances)
 
-coordinates_1, coordinates_2 = find_matching_coordinates(kp1, kp2, matches)
-average_feature_distance = calculate_mean_distance(coordinates_1, coordinates_2)
+def imu_data():
+    sensor = SenseHat()
+    gyro = sensor.get_gyroscope_raw()
+    accel = sensor.get_accelerometer_raw()
+    return gyro, accel
+
+def rotation_score(gyro):
+    return abs(gyro['x']) + abs(gyro['y']) + abs(gyro['z'])
+
+
+def capture_stable_image(filename):
+    """Capture image when rotation is stable"""
+    ROTATION_THRESHOLD = 0.1
+
+    for i in range(10) : #may cause a problem that the code will be stuck here
+
+        gyro, _ = imu_data()
+        rotation = rotation_score(gyro)
+
+        if rotation < ROTATION_THRESHOLD:
+            print(f"{filename} accepted, rotation ok")
+            return camera.take_photo(filename) # TOEGEVOEGD: maak daadwerkelijk foto
+        else:
+            print(f"{filename} rejected, rotation too high - retry")
+        
+def filter(distances,count): # Takes a list of distances, sorts it and gets the avarage distances of the middel 10 items.
+    distance = 0
+    length = len(distances)
+    distances.sort()
+
     for i in range(int(length/2)-int(count/2),int(length/2)+int(count/2)):
         distance += distances[i]
 
     return distance/10
-
-#average_feature_distance = calculate_mean_distance(coordinates_1, coordinates_2)
-
+    
+        
 
 def calculate_speed_in_kmps(feature_distance, GSD, time_difference):
     distance = feature_distance * GSD / 100000
     return distance / time_difference
 
 def format_speed(speed, digits):
+    print("Tdif"+str(speed))
     exponent = int(math.floor(math.log10(abs(speed))))
     decimals = digits - 1 - exponent
     return f"{speed:.{int(decimals)}f}"  # Gecorrigeerd: f-string met correct formatting
 
+def Write_to_file(data):
+    with open('result.txt', 'w') as file:
+           file.write(data)
 
 start_time = datetime.now()
 
 
 from picamzero import Camera
 
-cam = Camera()
+camera = Camera()
+
+start = datetime.now()
 
 
 
-image_1 = cam.take_photo("photo1.jpg")
-image_2 = cam.take_photo("photo2.jpg")  
+image_1 = capture_stable_image("photo1.jpg")
+image_2 = capture_stable_image("photo2.jpg")  
+
+speed_list = []
+images = []
 
 while (datetime.now()-start_time).total_seconds()  < 60: #600 sec
     
-    image_1 = cam.take_photo("photo1.jpg")
+    image_1 = capture_stable_image("photo1.jpg")
 
-    time.sleep(45)
+    if (datetime.now()-start).total_seconds()  > 30: # it takes 45 for the space station to do change the angel with 1° so 2/3 of that is enough diffrence to calculate the distances.
+        image_2 =  capture_stable_image("photo2.jpg") 
+        start = datetime.now()
 
-    image_2 = cam.take_photo("photo2.jpg")
-
-    images = []
-
-    speed_list = []
+    else:
         
-    time_difference = get_time_difference(image_1, image_2)
+        time_difference = get_time_difference(image_1, image_2)
 
-    image_1_cv, image_2_cv = convert_to_cv(image_1, image_2)
+        image_1_cv, image_2_cv = convert_to_cv(image_1, image_2)
 
-    keypoints_1, keypoints_2, descriptors_1, descriptors_2 = calculate_features(image_1_cv, image_2_cv, 1000)
-    matches = calculate_matches(descriptors_1, descriptors_2)
-
-    try:
-
-        coordinates_1, coordinates_2 = find_matching_coordinates(keypoints_1, keypoints_2, matches)
-
-        average_feature_distance = calculate_mean_distance(coordinates_1, coordinates_2)
-
-        GSD = 12648
-        speed = calculate_speed_in_kmps(average_feature_distance, GSD, time_difference)
-        speed_formatted = format_speed(speed, 5)
-
-        speed_list.append(speed_formatted)
-
-        avrage_speed = filter(speed_list,1)
-
-        #print(avrage_speed)
-
-        with open('result.txt', 'w') as file:
-           file.write(avrage_speed+"km/s")
+        keypoints_1, keypoints_2, descriptors_1, descriptors_2 = calculate_features(image_1_cv, image_2_cv, 1000)
+        matches = calculate_matches(descriptors_1, descriptors_2)
 
 
 
-    except:
-        print("Te weinig featers.")
+        try:
 
-    image_1 = image_2
+            coordinates_1, coordinates_2 = find_matching_coordinates(keypoints_1, keypoints_2, matches)
+
+            average_feature_distance = calculate_mean_distance(coordinates_1, coordinates_2)
+
+            GSD = 12648
+            speed = calculate_speed_in_kmps(average_feature_distance, GSD, time_difference)
+            speed_formatted = format_speed(speed, 5)
+
+            if speed_formatted != 0:
+                speed_list.append(speed_formatted)
+
+                avrage_speed = filter(speed_list,1)
+
+                #print(avrage_speed)
+
+
+                Write_to_file(str(int(avrage_speed)))
+                print("Write to file")
+
+
+
+        except:
+            print("Te weinig featers.")
+
+        image_1 = image_2
